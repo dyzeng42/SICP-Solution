f441f551448b934a6c1801c19430126390721211
zentorwie@gmail.com <zentorwie@gmail.com>
2014-12-21 14:52:04 +0800

    Change fold name to all-lower-case style

---
+++ chapter-2/section-2/exercise-2.2.rkt
@@ -0,0 +1,31 @@
+(define (make-seg start end)
+  (cons start end))
+
+(define (start-seg seg)
+  (car seg))
+
+(define (end-seg seg)
+  (cdr seg))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point z)
+  (car z))
+
+(define (y-point z)
+  (cdr z))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (midpoint-seg seg)
+  (let ((start (start-seg seg))
+        (end (end-seg seg)))
+    (make-point (/ (+ (average (x-point start)
+                               (y-point end))
+                      (average (x-point end)
+                               (y-point start)))
+                   2))))
+
+
